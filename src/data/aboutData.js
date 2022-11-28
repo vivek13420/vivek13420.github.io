38fb9bebac423dc3db3ca741aa7191f93fc78002
vivek13420 <vivekrana13420@gmail.com>
2022-11-28 22:11:50 +0530

    added resume

---
+++ src/data/aboutData.js
@@ -0,0 +1,8 @@
+export const aboutData = {
+  title: "Who I am",
+  description1:
+    "My name's Vivek. I'm a Full Stack developer based in Uttarakhand, India.",
+  description2:
+    " Looking forward to joining a company where I will be able to contribute towards individual and company growth.",
+  image: 2,
+};
---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Vivek Rana',
+    title: "Full Stack Web Developer",
+    desciption:"A Passionate aspiring Full Stack Developer skilled in HTML, CSS, JavaScript, React, Redux, moulded and shaped by Masai school’s intensive and immersive learning.",
+    image: 'https://avatars.githubusercontent.com/u/101565920?s=400&u=80b5ebe5bfec8fb6bc76919b08e2935255f400d4&v=4',
+    resumePdf: resume
+}
